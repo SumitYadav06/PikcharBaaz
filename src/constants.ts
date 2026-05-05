cbd7945a66f6f163febb7dfd2b19d12fdc832d69
SumitYadav06 <a1mausy06@gmail.com>
2026-05-05 08:21:20 +0530

    Delete src directory

--- src/constants.ts
+++
@@ -1,7 +0,0 @@
-export const TMDB_API_KEY = '5e872b180f9dfd43763e70c1eecd0d1a';
-export const TMDB_BASE_URL = 'https://api.themoviedb.org/3';
-export const TMDB_IMAGE_BASE = 'https://image.tmdb.org/t/p/w500';
-export const TMDB_IMAGE_ORIGINAL = 'https://image.tmdb.org/t/p/original';
-
-export const APP_DOWNLOAD_LINK = "https://www.appcreator24.com/app4020123-x0ey6g";
-export const WEB_LINK = "https://pikchar-baaz.vercel.app/";
